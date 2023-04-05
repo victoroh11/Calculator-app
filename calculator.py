1bc9725a08d89efb233ad81572e2079f0128ac28
victoroh11 <94450857+victoroh11@users.noreply.github.com>
2023-04-06 01:55:03 +0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+root = Tk()
+root.title("Calculator")
+
+num1 = Entry(root)
+num1.pack()
+
+num2 = Entry(root)
+num2.pack()
+
+result = StringVar()
+
+label = Label(root, textvariable=result)
+label.pack()
+
+rounding_scale = Scale(root, from_=0, to=10, orient=HORIZONTAL, label="Round up to:")
+rounding_scale.pack()
+
+def add():
+    result.set(float(num1.get()) + float(num2.get()))
+
+def subtract():
+    result.set(float(num1.get()) - float(num2.get()))
+
+def multiply():
+    result.set(float(num1.get()) * float(num2.get()))
+
+def divide():
+    result.set(float(num1.get()) / float(num2.get()))
+
+add_button = Button(root, text="+", command=add)
+add_button.pack()
+
+subtract_button = Button(root, text="-", command=subtract)
+subtract_button.pack()
+
+multiply_button = Button(root, text="*", command=multiply)
+multiply_button.pack()
+
+divide_button = Button(root, text="/", command=divide)
+divide_button.pack()
+
+root.mainloop()
